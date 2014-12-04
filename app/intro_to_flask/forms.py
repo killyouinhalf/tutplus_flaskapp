3f418c9d30a65b8d4f72501c2147f8bcbd2e2ede
killyouinhalf <klabeau@gmail.com>
2014-12-03 22:38:52 -0500

    Creating forms

--- app/intro_to_flask/forms.py
+++ app/intro_to_flask/forms.py
@@ -1,5 +1,5 @@
 from flask.ext.wtf import Form
-from wtforms import  StringField, TextAreaField, SubmitField, validators, ValidationError
+from wtforms import  StringField, TextAreaField, SubmitField, validators, ValidationError, PasswordField
 
 
 class ContactForm(Form):
@@ -8,3 +8,24 @@ class ContactForm(Form):
     subject = StringField("Subject", [validators.DataRequired("Please enter a subject.")])
     message = TextAreaField("Message", [validators.DataRequired("Please enter a message.")])
     submit = SubmitField("Send")
+
+class SignUpForm(Form):
+    firstname = StringField("First name", [validators.DataRequired("Please enter your first name.")])
+    lastname = StringField("Last name", [validators.DataRequired("Please enter your last name.")])
+    email = StringField("Email", [validators.DataRequired("Please enter your email address."), validators.Email('Please enter a valid email.')])
+    password = PasswordField('Password', [validators.DataRequired("Please select a password.")])
+    submit = SubmitField("Create account")
+
+    def __init__(self, *args, **kwargs):
+        Form.__init__(self. *args, **kwargs)
+
+    def validate(self):
+        if not Form.validate(self):
+            return False
+
+        user = User.query.filter_by(email = self.email.data.lower()).first()
+        if user:
+            self.email.errors.append("That email is already taken")
+            return False
+        else:
+            return True
